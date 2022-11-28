577fa362f6645ac1bedfbe7a57ed8422544967f1
Roger Mogas <rogermogas@gmail.com>
2022-11-28 16:59:11 +0100

    Created model.py file

---
+++ game_shazam/ml_logic/model.py
@@ -0,0 +1,33 @@
+
+
+def initialize_model(X):
+
+    model = None
+
+    return model
+
+def compile_model(model, learning_rate):
+
+    return model
+
+def train_model(model,
+                X,
+                y,
+                batch_size=64,
+                patience=2,
+                validation_split=0.3,
+                validation_data=None):
+
+    model = None
+    history = dict()
+
+    return model, history
+
+def evaluate_model(model,
+                   X,
+                   y,
+                   batch_size=64):
+
+    metrics = None
+
+    return metrics
