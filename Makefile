1b2826cf5f9c6508e1a2e30b3cb5b5bd9ec945e9
Roger Mogas <rogermogas@gmail.com>
2022-11-28 17:56:15 +0100

    created params.py file and implemented api logic

---
+++ Makefile
@@ -0,0 +1,2 @@
+run_api:
+	uvicorn game_shazam.api.fast:app --reload
---
+++ game_shazam/api/fast.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, File, UploadFile
+from game_shazam.ml_logic.registry import load_model
+from game_shazam.ml_logic.preprocessor import preprocess_features
+from game_shazam.ml_logic.params import GAMES_DICT
+import numpy as np
+from matplotlib.pyplot import imread
+
+app = FastAPI()
+app.state.model = load_model()
+
+
+@app.get("/")
+async def root():
+    return {"status": "ok"}
+
+
+@app.post("/files/")
+async def create_file(img: UploadFile=File(...)):
+    ### Receiving and decoding the image
+    contents = await img.read()
+
+    nparr = np.fromstring(contents, np.uint8)
+    np_img = imread(nparr)
+    pr_img = preprocess_features(np_img)
+    pred = app.state.model.predict(pr_img)
+    for i, p in enumerate(pred):
+        if p == 1:
+            return dict(prediction=GAMES_DICT[i])
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
