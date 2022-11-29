077769f0c8237c69b786e689af39ece21ff0798c
Mauro Vazquez Bassat <mvb.codes@gmail.com>
2022-11-29 12:03:49 +0100

    preprocess features using os

--- game_shazam/ml_logic/preprocessor.py
+++ game_shazam/ml_logic/preprocessor.py
@@ -1,8 +1,10 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from ml_logic.params import IMG_VECTOR
 import numpy as np
 from skimage.transform import resize
-from ml_logic.params import GAMES_DICT
+from ml_logic.params import GAMES_DICT, IMG_SIZE
 import os
+import cv2
 
 
 def get_data_generator(path:str,
@@ -14,9 +16,11 @@ def get_data_generator(path:str,
                        validation_split=0.3,
                        fill_mode='nearest',
                        batch_size=32,
-                       target_size=(224,224)
+                       target_size=IMG_SIZE
                        ):
     '''Get data generator to train on'''
+    print('\n   GETTING DATA GENERATOR\n')
+
     # Apply data augmentation
     datagen = ImageDataGenerator(
         rescale=rescale,
@@ -39,13 +43,18 @@ def get_data_generator(path:str,
 
 def preprocess_features(path:str):
 
+    data = []
+
     for f in GAMES_DICT.values:
         print(f'Fetching {f} images...')
         for filename in os.listdir(f):
 
             n_path = os.path.join(path, f, filename)
+            img = cv2.imread(n_path)
+            data.append(img)
 
+    resize_v = np.vectorize(resize(IMG_VECTOR))
+    data = resize_v(np.array(data))
 
-    X_preprocessed = None
-
-    return X_preprocessed
+    print('\n   DATA HAS BEEN PREPROCESSED\n')
+    return data
