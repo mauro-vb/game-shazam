15da80933b29c82cd43ae06c57bcbf0ea0c00d7e
Julian Rodriguez <jarodrig7@gmail.com>
2022-11-29 16:14:15 +0100

    test for Roger

--- scraping/main.py
+++ scraping/main.py
@@ -1,21 +1,15 @@
+import os
 from pytube import YouTube
 from pytube.cli import on_progress
 import get_frames
 from get_vid_ids import get_ids
-# from PyQt5 import
-
-# Not doing anything, call on_progress_callback=progress_funcin yt = Youtube
-def progress_func(self, stream, chunk, file_handle, bytes_remaining):
-    size = self.video.filesize
-    progress = (float(abs(bytes_remaining-size)/size))*float(100)
-    self.loadbar.setValue(progress)
-
+import sys
 
 ### MAIN ###
 
 # Admin input:
-game = "csgo"
-n_videos = 1
+game = "mortal kombat"
+n_videos = 2
 
 # Creating video id list:
 print("\n")
@@ -23,12 +17,21 @@ id_list = get_ids(game)
 id_list = id_list[:n_videos] # to correct: should limit no. of videos in funct, not at this stage
 print(f"Downloading and performing image extraction on {n_videos} videos...\n")
 
+output_path = f"imgs/{game}/"
+# Check if it exists
+itExist = os.path.exists(output_path)
+if not itExist:
+    # Create a new directory because it does not exist
+    os.makedirs(output_path)
+    print(f"New directory created for {game} images\n")
+
+
 # Downloading video and extracting frames for each downloaded video
 for each in id_list:
 
     try:
         print(f"Downloading video {each}...\n")
-        yt = YouTube(f'https://www.youtube.com/watch?v={each}', on_progress_callback=progress_func)
+        yt = YouTube(f'https://www.youtube.com/watch?v={each}')
         yt.streams.filter(
             progressive=True,
             file_extension='mp4').order_by('resolution').asc().first().download(
@@ -39,10 +42,10 @@ for each in id_list:
                 # These features could be changed by admin if needed
 
         print("Extracting frames...\n")
-        get_frames.extract_frames(f'temp_videos/{game}_{each}.mp4', f'imgs/{game}', ident=each)
+        get_frames.extract_frames(f'temp_videos/{game}_{each}.mp4', output_path, ident=each)
 
     except:
         continue
 
 print("---------------------\n")
-print(f"Finished generating images for '{game}' gameplay")
+print(f"Finished generating images for '{game}' gameplay\n")
