473b1fbe8d3e38124e6458ce661474266eb06456
Mauro Vazquez Bassat <mvb.codes@gmail.com>
2022-11-30 12:06:57 +0100

    minor changes

--- game_shazam/ml_logic/model.py
+++ game_shazam/ml_logic/model.py
@@ -1,6 +1,6 @@
 from tensorflow.keras import layers, models, optimizers
 from tensorflow.keras.callbacks import EarlyStopping
-from game_shazam.ml_logic.params import IMG_VECTOR
+from game_shazam.ml_logic.params import IMG_VECTOR, N_CATS
 from tensorflow.keras.applications.vgg16 import VGG16
 
 def initialize_model():
@@ -34,7 +34,7 @@ def initialize_model():
     model.add(layers.Dense(50, activation='relu'))
 
     ### Last layer - Classification
-    model.add(layers.Dense(10, activation='softmax'))
+    model.add(layers.Dense(N_CATS, activation='softmax'))
 
     return model
 
@@ -92,7 +92,7 @@ def initialize_VGG16_model(trainable=True):
     model.trainable = trainable
     flatten_layer = layers.Flatten()
     dense_layer = layers.Dense(200, activation='relu')
-    prediction_layer = layers.Dense(10, activation='softmax')
+    prediction_layer = layers.Dense(N_CATS, activation='softmax')
 
 
     model = models.Sequential([
@@ -101,5 +101,42 @@ def initialize_VGG16_model(trainable=True):
         dense_layer,
         prediction_layer
     ])
+
+    return model
+
+def initialize_big_model(trainable=True):
+
+    print('\n   INITIALIZING MODEL\n')
+
+    model = VGG16(weights="imagenet", include_top=False, input_shape=IMG_VECTOR)
+    model.trainable = trainable
+
+    model = models.Sequential()
+
+    ### Convolution & MaxPooling
+    # 1
+    model.add(layers.Conv2D(16, kernel_size=4, activation='relu', padding='same', input_shape=IMG_VECTOR))
+    model.add(layers.MaxPool2D(pool_size=(2, 2)))
+    # 2
+    model.add(layers.Conv2D(32, kernel_size=3, padding='same', activation='relu'))
+    model.add(layers.MaxPool2D(pool_size=(2, 2)))
+    # 3
+    model.add(layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'))
+    #model.add(layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'))
+    model.add(layers.MaxPool2D(pool_size=(2, 2)))
+    # 4
+    model.add(layers.Conv2D(128, kernel_size=2, activation='relu'))
+    model.add(layers.MaxPool2D(pool_size=(2, 2)))
+
+    ### Flattening
+    model.add(layers.Flatten())
+
+    ### One Fully Connected layer - "Fully Connected" is equivalent to saying "Dense"
+    model.add(layers.Dense(100, activation='relu'))
+
+    model.add(layers.Dense(50, activation='relu'))
+
+    ### Last layer - Classification
+    model.add(layers.Dense(N_CATS, activation='softmax'))
 
     return model
