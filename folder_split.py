776f0931bf1b1c1132e8569abb2495ef2b9ec5a3
Mauro Vazquez Bassat <mvb.codes@gmail.com>
2022-12-02 14:34:34 +0100

    just changed folder name

--- folder_split.py
+++ folder_split.py
@@ -3,7 +3,7 @@ import os
 # Split with a ratio.
 # To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
 
-folder = 'scrape_test'
+folder = 'initial_scrape'
 folder_path = os.path.join('data',folder)
 
 splitfolders.ratio(folder_path, output=folder_path + '_split',
---
+++ scraping_2/params.py
@@ -0,0 +1,19 @@
+import os
+
+GAMES = ['apex legends']
+
+FRAMES_PER_V = 500
+
+FRAMES_PER_G = 10000
+
+MAX_DURATION = 5
+
+API_KEY = 'AIzaSyCeCk-QRaS9-6HsAaSp52Wme4fHq29UPok'
+
+FRAMES_PER_MINUTE = 8
+
+VID_PATH = os.path.join(os.getcwd(),'data','tmp','')
+
+FRAMES_PATH = os.path.join(os.getcwd(),'data','scraped')
+
+VID_CROPPING = 12000 # in miliseconds
